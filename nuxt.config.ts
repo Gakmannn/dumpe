63ba434b27aa49a0e60236e3f03dad06168c403d
Nikolay Gakman <gakmannn@ya.ru>
2026-03-04 17:57:44 +0300

    fix pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/dumpe/', // Must start and end with a slash
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/image']
+  modules: ['@nuxt/fonts', '@nuxt/image'],
 })
